508b1d75e40fb9bcce2af3b2a28f06c659434678
Vibe Coding <fazligismail@gmail.com>
2025-08-21 23:40:32 +0800

    Add files via upload

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -6,13 +6,13 @@ REPO="https://raw.githubusercontent.com/vibecodingxx/vip/main/"
 # Cek apakah file ada
 if [ ! -f "$file_path" ]; then
     # Jika file tidak ada, buat file dan isi dengan dua baris
-    echo -e "XXX Server Connected\nGreen" | sudo tee "$file_path" > /dev/null
+    echo -e "XXX Server Connected\nGreen" | tee "$file_path" > /dev/null
     echo "File '$file_path' berhasil dibuat."
 else
     # Jika file ada, cek apakah isinya kosong
     if [ ! -s "$file_path" ]; then
         # Jika file ada tetapi kosong, isi dengan dua baris
-        echo -e "XXX Server Connected\nGreen" | sudo tee "$file_path" > /dev/null
+        echo -e "XXX Server Connected\nGreen" | tee "$file_path" > /dev/null
         echo "File '$file_path' kosong dan telah diisi."
     else
         # Jika file ada dan berisi data, tidak lakukan apapun
